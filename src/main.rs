546804b4fadd63d3f34d0ddc19b3a2c72548e142
Jost Roland <jostroland@yahoo.fr>
2023-08-10 00:46:45 +0000

    Mise en forme

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use std::io;
 
 fn main() {
     println!("Entrer votre premier nombre entier");
-    let a: u32 = 
-            read_var(&mut String::new())
-             .expect("Erreur de donnees saisies");
+    let a: u32 = read_var(&mut String::new())
+                    .expect("Erreur de donnees saisies");
 
     println!("Entrer votre second nombre entier");
-    let b: u32 = read_var(&mut String::new()).expect("Erreur de donnees saisies");
+    let b: u32 = read_var(&mut String::new())
+                    .expect("Erreur de donnees saisies");
 
     print!(" Le PGCD de {0} et {1} est {2}", a, b, pgcd(a, b));
 }
